40654f1568b48bcadcca74a123b314500accbc4b
weavora <hello@weavora.com>
2012-01-18 10:59:31 +0300

    Multiply form: delete link

--- examples/product/views/product/edit.php
+++ examples/product/views/product/edit.php
@@ -1,19 +1,29 @@
 <style type="text/css">
-	fieldset {
+	.form fieldset {
 		width: 300px;
+	}
+
+	.form .col {
+		width: 350px;
+		float: left;
+	}
+	.form .clear {
+		clear: both;
+		float: none;
 	}
 </style>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://raw.github.com/weavora/wform/master/js/jquery.multiplyforms.js"></script>
-<div class="form">
+
 <?php if ($product->isNewRecord):?>
 	<h1>Add Product</h1>
 <?php else:?>
 	<h1>Edit Product</h1>
 <?php endif;?>
 
+<div class="form">
 <?php $form = $this->beginWidget('WForm', array('htmlOptions' => array('enctype'=>'multipart/form-data'))); ?>
-	<?php if ($product->hasErrors()):?>
+<?php if ($product->hasErrors()):?>
 		<ul>
 			<?php foreach($product->getErrors() as $errors):?>
 				<?php if (is_array($errors)):?>
@@ -26,136 +36,135 @@
 			<?php endforeach;?>
 		</ul>
 	<?php endif;?>
-	<fieldset>
-		<legend>Product</legend>
-		<div class="row">
-			<?php echo $form->labelEx($product, 'name'); ?>
-			<?php echo $form->textField($product, 'name'); ?>
-			<?php echo $form->error($product, 'name'); ?>
-		</div>
-
-		<div class="row">
-			<?php echo $form->labelEx($product, 'price'); ?>
-			<?php echo $form->textField($product, 'price'); ?>
-			<?php echo $form->error($product, 'price'); ?>
-		</div>
-	</fieldset>
-
-	<fieldset>
-		<legend>Category</legend>
-		<div class="row">
-			<?php echo $form->labelEx($product, 'category_id'); ?>
-			<?php echo $form->dropDownList($product, 'category_id',
-				array_merge(CHtml::listData(Category::model()->findAll(), 'id', 'name'), array('' => 'new category'))
-			, array('empty' => 'none')) ?>
-			<?php echo $form->error($product, 'category_id'); ?>
-
-			<div style="display:none;" id="other-category">
-				<?php echo $form->labelEx($product, 'category.name'); ?>
-				<?php echo $form->textField($product, 'category.name', array('disabled' => 'disabled')); ?>
-				<?php echo $form->error($product, 'category.name'); ?>
+	<div class="col">
+		<fieldset>
+			<legend>Product</legend>
+			<div class="row">
+				<?php echo $form->labelEx($product, 'name'); ?>
+				<?php echo $form->textField($product, 'name'); ?>
+				<?php echo $form->error($product, 'name'); ?>
 			</div>
 
-		</div>
-	</fieldset>
-
-	<fieldset>
-		<legend>Tags</legend>
-		<div class="row">
-			<?php if ($tags): ?>
-				<ul class="tag-list">
-					<?php foreach ($tags as $index => $tag): ?>
-						<li>
-							<label>
-								<?php echo $tag->name ?>
-								<?php echo $form->checkBox($product, "tags.{$index}.id", array('value' => $tag->id, 'uncheckValue' => null)) ?>
-							</label>
-						</li>
-					<?php endforeach ?>
-				</ul>
-			<?php endif; ?>
-
-			<ul class="tags">
-				<?php if ($product->tags): ?>
-				<?php foreach ($product->tags as $index => $tag): ?>
-					<?php if ($tag->isNewRecord): ?>
-						<li>
-							<?php echo $form->textField($product, "tags.$index.name") ?>
-							<?php echo $form->error($product, "tags.$index.name") ?>
-							<a class="delete" href="#">delete</a>
-						</li>
-					<?php endif; ?>
-				<?php endforeach ?>
+			<div class="row">
+				<?php echo $form->labelEx($product, 'price'); ?>
+				<?php echo $form->textField($product, 'price'); ?>
+				<?php echo $form->error($product, 'price'); ?>
+			</div>
+		</fieldset>
+		<fieldset>
+			<legend>Description</legend>
+			<div class="row">
+				<?php echo $form->labelEx($product, 'description.color'); ?>
+				<?php echo $form->textField($product, 'description.color'); ?>
+				<?php echo $form->error($product, 'description.color'); ?>
+			</div>
+			<div class="row">
+				<?php echo $form->labelEx($product, 'description.size'); ?>
+				<?php echo $form->textField($product, 'description.size'); ?>
+				<?php echo $form->error($product, 'description.size'); ?>
+			</div>
+		</fieldset>
+		<fieldset>
+			<legend>Certificate</legend>
+			<div class="row">
+				<?php echo $form->labelEx($product, 'certificate.name'); ?>
+				<?php echo $form->textField($product, 'certificate.name'); ?>
+				<?php echo $form->error($product, 'certificate.name'); ?>
+				<?php if (!empty($product->certificate->image->file_origin)): ?>
+					<?php echo $form->hiddenField($product, "certificate.image.object_type") ?>
+					<?php echo $form->hiddenField($product, "certificate.image.id") ?>
+					<?php echo $form->hiddenField($product, "certificate.image.file") ?>
+					<?php echo $form->hiddenField($product, "certificate.image.file_origin") ?>
+					<?php echo $form->hiddenField($product, "certificate.image.tempFile") ?>
+					<?php echo CHtml::link($product->certificate->image->file_origin, $product->certificate->image->fileUrl) ?>
+					<a href="#" class="delete"">Delete</a>
+				<?php else: ?>
+					<?php echo $form->hiddenField($product, 'certificate.image.object_type', array('value' => Attachment::OBJECT_TYPE_CERTIFICATE)) ?>
+					<?php echo $form->fileField($product, 'certificate.image.file') ?>
 				<?php endif; ?>
-				<li style="display: none;" class="template" id="product-tag">
-					<?php echo $form->textField($product, 'tags..name') ?>
+			</div>
+		</fieldset>
+	</div>
+	<div class="col">
+		<fieldset>
+			<legend>Category</legend>
+			<div class="row">
+				<?php echo $form->labelEx($product, 'category_id'); ?>
+				<?php echo $form->dropDownList($product, 'category_id',
+					CHtml::listData($categories, 'id', 'name') + array('0' => 'new category')
+				, array('empty' => 'none')) ?>
+				<?php echo $form->error($product, 'category_id'); ?>
+
+				<div style="display:none;" id="other-category">
+					<?php echo $form->labelEx($product, 'category.name'); ?>
+					<?php echo $form->textField($product, 'category.name', array('disabled' => 'disabled')); ?>
+					<?php echo $form->error($product, 'category.name'); ?>
+				</div>
+
+			</div>
+		</fieldset>
+		<fieldset>
+			<legend>Tags</legend>
+			<div class="row">
+				<?php if ($tags): ?>
+					<ul class="tag-list">
+						<?php foreach ($tags as $index => $tag): ?>
+							<li>
+								<label>
+									<?php echo $tag->name ?>
+									<?php echo $form->checkBox($product, "tags.{$index}.id", array('value' => $tag->id, 'uncheckValue' => null)) ?>
+								</label>
+							</li>
+						<?php endforeach ?>
+					</ul>
+				<?php endif; ?>
+
+				<ul class="tags">
+					<?php if ($product->tags): ?>
+					<?php foreach ($product->tags as $index => $tag): ?>
+						<?php if ($tag->isNewRecord): ?>
+							<li>
+								<?php echo $form->textField($product, "tags.$index.name") ?>
+								<?php echo $form->error($product, "tags.$index.name") ?>
+								<a class="delete" href="#">delete</a>
+							</li>
+						<?php endif; ?>
+					<?php endforeach ?>
+					<?php endif; ?>
+					<li style="display: none;" class="template" id="product-tag">
+						<?php echo $form->textField($product, 'tags..name') ?>
+						<a class="delete" href="#">delete</a>
+					</li>
+				</ul>
+				<a id="add-tag" href="#">add</a>
+			</div>
+		</fieldset>
+		<fieldset>
+			<legend>Images</legend>
+			<ul class="images">
+				<?php if ($product->images): ?>
+					<?php foreach ($product->images as $index => $image): ?>
+						<?php if (!empty($image->file_origin)):?>
+							<?php echo $form->hiddenField($product, "images.{$index}.object_type") ?>
+							<?php echo $form->hiddenField($product, "images.{$index}.id") ?>
+							<?php echo $form->hiddenField($product, "images.{$index}.file") ?>
+							<?php echo $form->hiddenField($product, "images.{$index}.file_origin") ?>
+							<?php echo $form->hiddenField($product, "images.{$index}.tempFile") ?>
+							<?php echo CHtml::link($image->file_origin, $image->fileUrl) ?>
+							<a href="#" class="delete"">Delete</a>
+						<?php endif; ?>
+					<?php endforeach ?>
+				<?php endif; ?>
+				<li style="display: none;" class="template" id="product-image">
+					<?php echo $form->hiddenField($product, 'images..object_type', array('value' => Attachment::OBJECT_TYPE_PRODUCT_IMAGE)) ?>
+					<?php echo $form->fileField($product, 'images..file') ?>
+					<a href="#" class="delete"">Delete</a>
 				</li>
 			</ul>
-			<a id="add-tag" href="#">add</a>
-
-		</div>
-	</fieldset>
-
-	<fieldset>
-		<legend>Images</legend>
-		<ul class="images">
-			<?php if ($product->images): ?>
-				<?php foreach ($product->images as $index => $image): ?>
-					<?php if (!empty($image->file_origin)):?>
-						<?php echo $form->hiddenField($product, "images.{$index}.object_type") ?>
-						<?php echo $form->hiddenField($product, "images.{$index}.id") ?>
-						<?php echo $form->hiddenField($product, "images.{$index}.file") ?>
-						<?php echo $form->hiddenField($product, "images.{$index}.file_origin") ?>
-						<?php echo $form->hiddenField($product, "images.{$index}.tempFile") ?>
-						<?php echo CHtml::link($image->file_origin, $image->fileUrl) ?>
-						<a href="#" class="delete"">Delete</a>
-					<?php endif; ?>
-				<?php endforeach ?>
-			<?php endif; ?>
-			<li style="display: none;" class="template" id="product-image">
-				<?php echo $form->hiddenField($product, 'images..object_type', array('value' => Attachment::OBJECT_TYPE_PRODUCT_IMAGE)) ?>
-				<?php echo $form->fileField($product, 'images..file') ?>
-			</li>
-		</ul>
-		<a id="add-image" href="#">add</a>
-
-	</fieldset>
-
-	<fieldset>
-		<legend>Certificate</legend>
-		<div class="row">
-			<?php echo $form->labelEx($product, 'certificate.name'); ?>
-			<?php echo $form->textField($product, 'certificate.name'); ?>
-			<?php echo $form->error($product, 'certificate.name'); ?>
-			<?php if (!empty($product->certificate->image->file_origin)): ?>
-				<?php echo $form->hiddenField($product, "certificate.image.object_type") ?>
-				<?php echo $form->hiddenField($product, "certificate.image.id") ?>
-				<?php echo $form->hiddenField($product, "certificate.image.file") ?>
-				<?php echo $form->hiddenField($product, "certificate.image.file_origin") ?>
-				<?php echo $form->hiddenField($product, "certificate.image.tempFile") ?>
-				<?php echo CHtml::link($product->certificate->image->file_origin, $product->certificate->image->fileUrl) ?>
-				<a href="#" class="delete"">Delete</a>
-			<?php else: ?>
-				<?php echo $form->hiddenField($product, 'certificate.image.object_type', array('value' => Attachment::OBJECT_TYPE_CERTIFICATE)) ?>
-				<?php echo $form->fileField($product, 'certificate.image.file') ?>
-			<?php endif; ?>
-		</div>
-	</fieldset>
-
-	<fieldset>
-		<legend>Description</legend>
-		<div class="row">
-			<?php echo $form->labelEx($product, 'description.color'); ?>
-			<?php echo $form->textField($product, 'description.color'); ?>
-			<?php echo $form->error($product, 'description.color'); ?>
-		</div>
-		<div class="row">
-			<?php echo $form->labelEx($product, 'description.size'); ?>
-			<?php echo $form->textField($product, 'description.size'); ?>
-			<?php echo $form->error($product, 'description.size'); ?>
-		</div>
-	</fieldset>
-
+			<a id="add-image" href="#">add</a>
+		</fieldset>
+	</div>
+	<div class="clear"></div>
 	<div class="row submit">
 		<?php echo CHtml::submitButton('Save'); ?>
 	</div>
@@ -168,11 +177,11 @@
 	$(document).ready(function(){
 		$('#ProductForm_category_id').change(function(){
 			if (this.options.length == (this.selectedIndex + 1)) {
-			    $('#other-category')
-			    	.find('input')
-			    		.removeAttr('disabled')
-			    	.end()
-			    .show();
+				$('#other-category')
+					.find('input')
+						.removeAttr('disabled')
+					.end()
+				.show();
 			} else {
 				$('#other-category')
 					.find('input')
--- js/jquery.multiplyforms.js
+++ js/jquery.multiplyforms.js
@@ -1,76 +1,83 @@
 (function($){
-    $.multiplyForms = function(el, options){
-        var self = this;
-        self.$el = $(el);
-        self.el = el;
-        self.formCount = 100;
+	$.multiplyForms = function(element, options){
+		var self = this;
+		self.element = $(element);
+		self.formCounter = 100;
+		self.options = $.extend({}, $.multiplyForms.defaultOptions, options);
 
-        self.$el.data("multiplyForms", self);
+		self.element.data("multiplyForms", self);
 
-        self.init = function() {
-            self.options = $.extend({},$.multiplyForms.defaultOptions, options);
+		self.init = function() {
+
 			$(self.options.template).find('input, textarea, select, button').attr('disabled', 'disabled');
 
 			// find add links inside self.element
-			var $addLinks = self.$el.find(self.options.addLink);
+			var addLinks = self.element.find(self.options.addLink);
 			// find add links in document
-			$addLinks = $addLinks.length ? $addLinks : $(self.options.addLink);
+			addLinks = addLinks.length ? addLinks : $(self.options.addLink);
 
-			$addLinks.click(function(e) {
+			addLinks.click(function(e) {
 				e.preventDefault();
-				var $newElement = self.$el.find(self.options.template)
-					.clone(false)
-					.find('input, textarea, select, button')
-						.removeAttr('disabled')
-					.end()
-				.appendTo(self.el).show();
-				// if options.template is className then remove class
-				if (typeof self.options.template == "string" && self.options.template.indexOf('.') == 0) {
-				    $newElement.removeClass(self.options.template.replace('.', ' '));
-				}
-
-				self._updateIndex($newElement);
-				// afterAdd callback
-				if (typeof(self.options.afterAdd) == 'function') {
-				    self.options.afterAdd.call(this, $newElement);
-				}
+				self._cloneTemplate();
 			});
-        };
 
-        self._updateIndex = function($form) {
-        	var formId = $form.attr('id') + "_" + self.formCount;
-        	$form.attr('id', formId);
-        	$form.find('*[name*="{index}"]').each(function() {
-				$(this).attr('name', $(this).attr('name').replace('{index}', self.formCount));
-				this.id = this.id.replace('{index}', self.formCount);
-			});
-			$form.find(self.options.deleteLink).click(function(e) {
+			self.element.find(self.options.deleteLink).live('click', function(e) {
 				e.preventDefault();
-				var $element = $('#'+formId);
+
+				var embedForm = $(e.target).parents("." + self.options.embedClass);
+
 				// beforeDelete callback
-				if (typeof(self.options.beforeDelete) == 'function') {
-				    self.options.beforeDelete.call(this, $element);
+				if ($.isFunction(self.options.beforeDelete)) {
+					self.options.beforeDelete.call(this, embedForm, self);
 				}
-				$element.remove();
+				embedForm.remove();
 			});
-			self.formCount++;
-        };
+		};
 
-        self.init();
-    };
+		self._cloneTemplate = function() {
+			var self = this;
+			var newForm = self.element.find("." + self.options.templateClass)
+				.clone(false)
+				.find('input, textarea, select, button')
+					.removeAttr('disabled')
+				.end()
+				.appendTo(self.element)
+				.addClass(self.options.embedClass)
+				.removeClass(self.options.templateClass)
+				.show();
 
-    $.multiplyForms.defaultOptions = {
-    	addLink: ".add",
-    	deleteLink: ".delete",
-    	template: ".template",
-    	afterAdd: undefined,
-    	beforeDelete: undefined
-    };
+			self._updateIndex(newForm);
 
-    $.fn.multiplyForms = function(options) {
-        return this.each(function(){
-            (new $.multiplyForms(this, options));
-        });
-    };
+			// afterAdd callback
+			if ($.isFunction(self.options.afterAdd)) {
+				self.options.afterAdd.call(this, newForm, self);
+			}
+		};
+
+		self._updateIndex = function(form) {
+			form.find('*[name*="{index}"]').each(function() {
+				$(this).attr('name', $(this).attr('name').replace('{index}', self.formCounter));
+				this.id = this.id.replace('{index}', self.formCounter);
+			});
+			self.formCounter++;
+		};
+
+		self.init();
+	};
+
+	$.multiplyForms.defaultOptions = {
+		addLink: ".add",
+		deleteLink: ".delete",
+		templateClass: "template",
+		embedClass: "embed",
+		afterAdd: undefined,
+		beforeDelete: undefined
+	};
+
+	$.fn.multiplyForms = function(options) {
+		return this.each(function(){
+			(new $.multiplyForms(this, options));
+		});
+	};
 
 })(jQuery);
